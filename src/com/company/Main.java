e2ae64da6c56f5c8909117a7627847a008592cc0
mindaugasinas <sinkeviciusm@gmail.com>
2018-01-30 15:42:32 +0200

    Penkta pamoka (projekto pabaiga)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Iveskite du skaicius");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        System.out.println("Skaiciu suma: " + Suma(a, b));
+        System.out.println("Skaiciu skirtumas: " + Skirtumas(a, b));
+        System.out.println("Skaiciu suma per float: " + Suma((float)a, (float)b));
+        System.out.println("Skaiciu skirtumas per float: " + Skirtumas((float)a, (float)b));
+    }
+
+    public static int Suma(int a, int b) {
+        return a + b;
+    }
+
+    public static int Skirtumas(int a, int b) {
+        return a - b;
+
+    }public static float Suma(float a, float b) {
+        return a + b;
+    }
+
+    public static float Skirtumas(float a, float b) {
+        return a - b;
     }
 }
